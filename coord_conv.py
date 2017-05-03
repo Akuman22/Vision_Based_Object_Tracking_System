69eac2456472dcfdee165139ef190083b769100c
Akuman22 <amehta22@bu.edu>
2017-05-03 01:44:51 -0400

    V2
    Changes in alues

--- coord_conv.py
+++ coord_conv.py
@@ -10,8 +10,8 @@ pub = rospy.Publisher('coord_send',UInt16MultiArray , queue_size=10)
 def callback(data):
     x = data.data[0]
     y = data.data[1]
-    x = (100.0/480.0)*x
-    y = (100.0/640.0)*y
+    x = (100.0/1100.0)*(338 - x) 
+    y = (100.0/1100.0)*(403 - y)
     r = np.sqrt((x*x)+(y*y))
     if(y!=0):
         th = math.degrees(math.atan(x/y))
